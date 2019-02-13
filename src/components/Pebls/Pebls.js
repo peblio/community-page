54ea8c1772e48ea49804f1f3ae158ec5fc95fb96
mathuramg <mmg9291@gmail.com>
2019-02-13 15:05:37 +0530

    fix pagination offset

--- src/components/Pebls/Pebls.js
+++ src/components/Pebls/Pebls.js
@@ -24,17 +24,20 @@ class Pebls extends Component {
 
   increasePageOffset = () => {
     this.setState({
-      offset: this.state.offset+1
+      offset: this.state.offset+this.state.pageLimit
     })
   }
   retrievePeblsWithTag=(tag) =>{
     this.props.setTagName(tag);
     this.props.getPeblsFromTag(tag, this.state.pageLimit, this.state.offset);
+    this.increasePageOffset();
   }
 
   fetchMoreData = () => {
     console.log('in here ??')
+    console.log(this.state.offset)
     this.props.getPeblsFromTag(this.props.tagName, this.state.pageLimit, this.state.offset);
+    this.increasePageOffset();
   }
 
   componentWillUpdate(nextProps) {
